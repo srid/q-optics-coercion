84a1f569eb926dcd4090878562e39ad641472786
Sridhar Ratnakumar <srid@srid.ca>
2022-03-15 18:03:17 -0400

    poc

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Main where
 
 import Main.Utf8 (withUtf8)
+import Optics.Core
+
+newtype Stringable a = Stringable a
+  deriving newtype (ToString, IsString)
+
+newtype MyPrism a
+  = MyPrism (Prism' FilePath a)
+
+-- MyPrism (a -> String, String -> Maybe a)
+
+class MkPrism a where
+  mkPrism :: MyPrism a
+
+instance (IsString a, ToString a) => MkPrism (Stringable a) where
+  mkPrism = error "not implemented"
+
+newtype Foo = Foo Text
+  deriving newtype (IsString, ToString)
+  deriving (MkPrism) via (Stringable Foo)
 
 main :: IO ()
 main = do
